ae0505911da5fd4aeb0a7a036623b3ed83588aa5
Kelsie Merchant <eislek02@gmail.com>
2020-04-07 16:54:02 -0500

    Update v1.0 to debug and include comments for future debugging

--- shelly.c
+++ shelly.c
@@ -9,40 +9,49 @@ int main(void)
 {
 	char *prompt = "$ ";
 	char *buffer;
-	size_t BUFF_SIZE = 1024;
+	size_t BUFF_SIZE = 4;
 	ssize_t rd = 0;
 	const char *space = " ";
 	char **args;
-	int check = 0, i = 0;
+	int check = 0;
+	int i = 0;
 
-	while (check == 0)
+	while (check != 3)
 	{
-		/* writes prompt to stdout */
-		write(STDOUT_FILENO, prompt, 2);
-
 		/* dynamically allocate memory for buffer */
 		buffer = malloc(sizeof(char) * BUFF_SIZE);
 		if (buffer == NULL)
 			malloc_error();
+/*		printf("We have malloced our buffer\n");
+ */
+		/* writes prompt to stdout */
+		write(STDOUT_FILENO, prompt, 2);
 
 		/* read to buffer using getline function */
-		rd = getline(&buffer, &BUFF_SIZE, stdin);
+		rd = _getline(&buffer, &BUFF_SIZE, stdin);
 		if (rd < 0)
 		{
 			free(buffer);
 			getline_error();
 		}
-
+		printf("We have read %zu bytes. Here's what we read:\n", rd);
+		printf("%s", buffer);
 		/* parses the function based on delim, create args like argv */
 		args = _parse(buffer, space);
 		free(buffer);
+		printf("Here's how Shelly sets args\n");
+		for (i = 0; args[i]; i++)
+			printf("%s\n", args[i]);
 
 		/* fork into parent and child processes to execute program */
 		execute(args);
+		printf("Awesome!  We finished executing program %s\n", args[0]);
 		for (i = 0; args[i]; i++)
 			free(args[i]);
 		free(args[i]);
 		free(args);
+		printf("Now everything should be free.  Let's go again!\n");
+		check++;
 	}
 	return (0);
 }
