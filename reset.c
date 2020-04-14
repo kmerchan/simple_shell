e7571efc8ca5b7916462f66203d03d39a8e921cc
Kelsie Merchant <eislek02@gmail.com>
2020-04-14 12:04:57 -0500

    Fix typos in code and turn off comments in reset function

--- reset.c
+++ reset.c
@@ -17,8 +17,8 @@ char *reset(char **buffer, char ***args, const char *delim)
 	char *newbuffer;
 	int i = 0, j = 0;
 
-	printf("Here's tempbuffer: %s\n", tempbuffer);
-	for (i = 0; (*args)[i]; i++)
+/*	printf("Here's tempbuffer: %s\n", tempbuffer);
+ */	for (i = 0; (*args)[i]; i++)
 	{
 		while (tempbuffer[0] == delim[0])
 			tempbuffer++;
@@ -26,12 +26,10 @@ char *reset(char **buffer, char ***args, const char *delim)
 			tempbuffer++;
 		while (tempbuffer[0] == delim[0])
 			tempbuffer++;
-		printf("Here's tempbuffer: %s\n", tempbuffer);
-	}
-	printf("Before tmpbuffer nullbyt check\n");
+/*		printf("Here's tempbuffer: %s\n", tempbuffer);
+ */	}
 	if (tempbuffer[0] == '\0')
 		return (NULL);
-	printf("After tmp buffer nullbyte check\n");
 	newbuffer = malloc(sizeof(char) * (_strlen(tempbuffer) + 1));
 	if (newbuffer == NULL)
 	{
